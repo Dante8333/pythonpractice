e9bdb0a2f1a9a97dfd055c7882b1299327e35e17
dante <apattamsetty2022@fau.edu>
2022-09-12 09:48:28 -0400

    first comit

---
+++ basic_math.py
@@ -0,0 +1,15 @@
+# addition, substraction , multiplication , divisionef basic_math():
+a=2
+b=3
+def maths():
+   add = a + b
+   sub = a - b
+   mul = a * b
+   print(add)
+   print(sub)
+
+maths()
+
+ 
+
+
---
+++ init.py
@@ -0,0 +1,9 @@
+# addition, substraction , multiplication , division 
+def basic_math(a,b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a / b)
+
+basic_math(4,5)
+
