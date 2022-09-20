5f539016f0d97ca216a0f6b1a3c89cdf0767daa8
dante <apattamsetty2022@fau.edu>
2022-09-19 23:03:45 -0400

    newfile

---
+++ polymorphism.py
@@ -0,0 +1,19 @@
+class full_stack():
+    def coding_language(self):
+        print("language used: reactjs")
+class web_development():
+    def coding_language(self):
+        print("language used: python")
+class mobile_application():
+    def coding_language(self):
+        print("language used: swift")
+
+full_stack = full_stack()
+web_development = web_development()
+mobile_application = mobile_application()
+
+full_stack.coding_language()
+
+web_development.coding_language()
+
+mobile_application.coding_language()
